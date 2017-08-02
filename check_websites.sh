8546d4f288c3a74dceeb30027968d1d71cdccc5c
root <root@Alucard>
2017-08-02 12:53:09 +0000

    Change check method & add restart as a parameter

--- check_websites.sh
+++ check_websites.sh
@@ -48,10 +48,13 @@
 #   - 2017-07-28: v1.0 - First release.
 
 # Set the name of the docker's front-end network :
-DOCKER_FRONTEND_NETWORK=traefik-proxy
+DOCKER_FRONTEND_NETWORK=traefik_proxy
 
 # Path of the telegram-notify.sh script.
 TELEGRAM_PATH=/data/scripts/telegram-notify.sh
+
+# If a website is KO, do I restart the container ?
+RESTART=yes
 
 # LOGS_PATH = Path where logs will be stored
 LOGS_PATH=/var/log
@@ -85,37 +88,38 @@ echo "|" >> $LOG_TEMP
 # Check all running containers
 for i in $(docker network inspect $DOCKER_FRONTEND_NETWORK | grep Name | sed 1d | awk -F\" '{print $4}')
  do
-   # Get url associated to the container from traefik label "traefik.frontend.rule" or from environnement variable "VIRTUAL_HOST" if using nginx
-   URL=$(docker inspect $i | grep cw.check.url | awk -F"\"" '{print $4}')
-   if [ -z "$URL" ]; then
-     URL=$(docker inspect $i | grep traefik.frontend.rule | awk -F":" '{print $3}' | awk -F\" '{print $1}')
-     if [ -z "$URL" ]; then
-       URL=$(docker inspect $i | grep VIRTUAL_HOST | awk -F"=" '{print $2}' | awk -F\" '{print $1}')
-     fi
+   # Get IP address and port from docker
+   CONT_IP=$(docker inspect -f '{{ .NetworkSettings.Networks.$DOCKER_FRONTEND_NETWORK.IPAddress }}' $i)
+   if [ -z "$CONT_IP" ]; then
+     CONT_IP=$(docker inspect $i | grep VIRTUAL_PORT | awk -F"=" '{print $2}' | awk -F\" '{print $1}')
    fi
+   CONT_PORT=$(docker inspect $i | grep traefik.backend.port | awk -F"\"" '{print $4}')
    DOCKER_SCRIPT_LOG_FILE=$LOGS_PATH/cw_$i.log
+   
    if [ ! -z "$URL" ]; then
      # If there is a URL on that container, get http code associated
-     RESULT=$(curl -s -o /dev/null -w "%{http_code}" -L $URL)
-     if [ "$RESULT" != "200" ]; then
-	   # If website is KO, restart the container
+     RESULT=$(curl -s -o /dev/null -w "%{http_code}" -L $CONT_IP:CONT_PORT)
+     if [ "$RESULT" != "200" ] && [ "RESTART" == "yes" ]; then
+       # If website is KO, restart the container
        echo -e "[ ${CRED}KO$CEND ] ${CYELLOW}https://$URL$CEND down with code $RESULT. Try restarting container $i..." >> $LOG_TEMP
        docker restart $i >/dev/null
        sleep 10
        RESULT2=$(curl -s -o /dev/null -w "%{http_code}" -L $URL)
-	   # If website is still KO after a container restart, notify through telegram with container's logs
+       # If website is still KO after a container restart, notify through telegram with container's logs
        if [ "$RESULT2" != "200" ]; then
          echo -e "[ ${CRED}ERR$CEND ] ${CYELLOW}https://$URL$CEND is still ${CRED}KO$CEND with http code : $RESULT2. Please check $i !" >> $LOG_TEMP
          if [ ! -f $NOTIFIED_LIST ] || [[ ! " $(cat $NOTIFIED_LIST) " =~ " $i " ]]; then
-		   docker logs $i > $DOCKER_SCRIPT_LOG_FILE 2>/dev/null
-           $TELEGRAM_PATH --error --text "https://"$URL" is DOWN\nhttp code "$RESULT2"\nPlease check "$i" logs" --document $DOCKER_SCRIPT_LOG_FILE
+           docker logs $i > $DOCKER_SCRIPT_LOG_FILE 2>/dev/null
+           $TELEGRAM_PATH --error --text "https://"$URL" is DOWN\nhttp code "$RESULT2"\nPlease check logs.\n "$i" restarted unsuccessfully." --document $DOCKER_SCRIPT_LOG_FILE
            echo $i >> $NOTIFIED_LIST
          fi
-	   else
-	     # If website if OK after a container's restart, notify it through telegram
-	     $TELEGRAM_PATH --success --text "https://"$URL" was KO, but is OK after $i's restart"
+       else
+         # If website if OK after a container's restart, notify it through telegram
+         $TELEGRAM_PATH --success --text "https://"$URL" was KO, but is OK after $i's restart"
        fi
-     else
+	 if [ "$RESULT" != "200" ] && [ "RESTART" == "no" ]; then
+	   $TELEGRAM_PATH --error --text "https://"$URL" is DOWN\nhttp code "$RESULT2"\nPlease check logs." --document $DOCKER_SCRIPT_LOG_FILE
+     elif [ "$RESULT" == "200" ]; then
        echo -e "[ ${CGREEN}OK$CEND ] https://$URL is up." >> $LOG_TEMP
        while read line; do
          if [ $line = "$i" ]; then
@@ -144,4 +148,4 @@ if [ ! -s $NOTIFIED_LIST ]
 then
   # If previously notified list is empty, remove file
   rm -f $NOTIFIED_LIST
-fi
+fi 
